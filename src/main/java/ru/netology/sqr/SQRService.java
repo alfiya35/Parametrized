8116cab67caf465660dc83ba2b22e1667a74ff68
Альфия Устименко <alfiya.ustimenko@ukr.net>
2022-04-13 14:28:45 +0500

    initial commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,14 +4,12 @@ public class SQRService {
 
     public int calculateAmountSquare(int rangeLimit1, int rangelimit2) {
         for (int i = 10; i <= 99; i++) {
-            int counter;
-            if (counter >= rangeLimit1 && counter <= rangelimit2)
-                counter++;
-            {
-
+            int counter = i * i;
+            if (counter >= rangeLimit1 && counter <= rangelimit2) ;
+            counter++; {
                 return counter;
             }
-
+        }
+            return 0;
         }
     }
-}
