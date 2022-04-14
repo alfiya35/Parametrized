bbbc267c949bbbc19da7a5092c9583618752e91f
Альфия Устименко <alfiya.ustimenko@ukr.net>
2022-04-14 21:37:03 +0500

    initial commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,15 +1,16 @@
 package ru.netology.sqr;
 
 public class SQRService {
-
     public int calculateAmountSquare(int rangeLimit1, int rangelimit2) {
-
+        int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (counter >= rangeLimit1 && counter <= rangelimit2) ;
-            counter++; {
-                return counter;
+            int sqr = i * i;
+            if (sqr >= rangeLimit1 && sqr <= rangelimit2) {
+                count++;
             }
+
         }
-            return 0;
-        }
+        return count;
     }
+
+}
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -16,4 +16,39 @@ class SQRServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    void shouldCalculateAmountSquareNothing() {
+        SQRService service = new SQRService();
+        int rangeLimit1 = 5;
+        int rangeLimit2 = 99;
+        int expected = 0;
+
+        int actual = service.calculateAmountSquare(5, 99);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateAmountSquareNothingMax() {
+        SQRService service = new SQRService();
+        int rangeLimit1 = 100;
+        int rangeLimit2 = 9801;
+        int expected = 90;
+
+        int actual = service.calculateAmountSquare(100, 9900);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateAmountSquareNothingOne() {
+        SQRService service = new SQRService();
+        int rangeLimit1 = 5;
+        int rangeLimit2 = 100;
+        int expected = 1;
+
+        int actual = service.calculateAmountSquare(5, 100);
+
+        assertEquals(expected, actual);
+    }
+
+
 }
